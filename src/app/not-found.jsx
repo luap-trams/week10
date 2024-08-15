ce4726411da5610c9affe50420b76ab850eec481
luap-trams <luap.trams@gmail.com>
2024-08-15 12:43:42 +0100

    fix: add &apos; in h1 element

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NotFound() {
   return (
     <div className='bg-slate-900 h-screen flex flex-col items-center justify-center text-white text-4xl uppercase font-bold'>
-        <h1>sorry, the page you're looking for doesn&apos;t exist yet</h1>
+        <h1>sorry, the page you're looking for does not exist yet</h1>
     </div>
   )
 }
