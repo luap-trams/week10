eb692a2a24141a4a479e27888954fb60b7a5c03a
luap-trams <luap.trams@gmail.com>
2024-08-18 10:53:12 +0100

    style: change index backgroud from white to amber

---
+++ src/app/about/layout.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: "About | Your Trusted Partner in Real Estate Management",
+    description: "Invest | Find your dream home today",
+    keywords: 'home ownership, apartment rental, sales'
+};
+
+
+
+import React from 'react'
+export default function layout() {
+  return (
+    <div>layout</div>
+  )
+}
